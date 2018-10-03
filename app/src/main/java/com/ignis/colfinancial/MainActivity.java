0a003c07eb0d89555f1d4d2f0e526dcb0ea2611d
ikindred <kindred.inocencio@gmail.com>
2018-10-03 12:52:37 +0800

    done login

--- app/src/main/java/com/ignis/colfinancial/MainActivity.java
+++ app/src/main/java/com/ignis/colfinancial/MainActivity.java
@@ -1,11 +1,17 @@
 package com.ignis.colfinancial;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,6 +20,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -24,9 +38,19 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.security.MessageDigest;
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+
+    CallbackManager callbackManager;
+    ProgressDialog mDialog;
 
 
     private LinearLayout profile_section;
@@ -36,6 +60,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView prof_pic;
     private GoogleApiClient googleApiClient;
     private static final int REQ_CODE = 9001;
+    int a = 0;
 
 
     @Override
@@ -59,8 +84,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         sign_in.setOnClickListener(this);
 
 
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.facebook_login);
+        loginButton.setReadPermissions(Arrays.asList("public_profile","email"));
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                mDialog = new ProgressDialog(MainActivity.this);
+                mDialog.setMessage("Retrieving data...");
+                mDialog.show();
+
+                String accesstoken = loginResult.getAccessToken().getToken();
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        mDialog.dismiss();
+                        Log.d("response", response.toString());
+                      getData(object);
+                    }
+                });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields","id,name,email");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+                profile_section.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+        //if ready
+        if(AccessToken.getCurrentAccessToken() != null){
+            email.setText(AccessToken.getCurrentAccessToken().getUserId());
+        }
+
+
+       // printKeyHash();
     }
 
+
+    /*private void printKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.ignis.colfinancial", PackageManager.GET_SIGNATURES);
+
+            for (Signature signature : info.signatures) {
+
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+
+        } catch(Exception e) {
+        }
+    }*/
 
     @Override
     public void onClick(View v) {
@@ -95,6 +183,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Toast.makeText(this, "signin", Toast.LENGTH_SHORT).show();
         Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
         startActivityForResult(intent, REQ_CODE);
+        a = 1;
     }
 
     private void handle_result(GoogleSignInResult result) {
@@ -138,16 +227,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        Toast.makeText(this, ""+resultCode, Toast.LENGTH_SHORT).show();
-        if (resultCode == -1) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-             handle_result(result);
+        if(a == 1) {
+            Toast.makeText(this, "" + resultCode, Toast.LENGTH_SHORT).show();
+            if (resultCode == -1) {
+                GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+                handle_result(result);
 
+                a = 0;
+            }
         }
+
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void getData(JSONObject object) {
+
+        try {
+
+            URL profile_picture = new URL("https://graph.facebook.com/"+object.getString("id")+"/picture?width=90&height=90");
+
+            Picasso.with(this).load(profile_picture.toString()).into(prof_pic);
+
+            email.setText(object.getString("email"));
+          //  birthday.setText(object.getString("birthday"));
+           // friends.setText("Friends: "+object.getJSONObject("friends").getJSONObject("summary").getString("total_count"));
+            name.setText(object.getString("name"));
+
+           // Glide.with(this).load(profile_picture).into(prof_pic);
+
+        } catch(Exception e) {
+        }
+
+
     }
 }
